7b2cb7e153793eba708ef59abc887d1159ab4291
kleshninpo <kleshnin.p.o@gmail.com>
2018-12-19 17:47:49 +0300

    +

---
+++ src/data/logosHeader.js
@@ -0,0 +1,7 @@
+
+const logosHeader = [
+    {id: 1, src: './static/img/logo-star.png', about: 'Звезда', href: 'https://star.ru', text: 'Звездный блог'},
+    {id: 2, src: './static/img/logo-youtube.png', about: 'Youtube', href: 'https://youtube.ru', text: 'Смотрите нас на YouTube'},
+    {id: 3, src: './static/img/logo-youtube2.png', about: 'Youtube', href: 'https://youtube.ru', text: 'Смотрите наш прямой эфир'},
+];
+export default logosHeader;
---
+++ src/data/logosMain.js
@@ -0,0 +1,5 @@
+const logosMain = [
+    {id: 1, src: '../img/logo.png', about: 'Shop24', text: ''},
+    {id: 2, src: '../img/logo-gray.png', about: 'Shop24', text: ''}
+]
+export default logosMain;
