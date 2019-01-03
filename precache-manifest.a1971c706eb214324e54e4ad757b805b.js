b0f3dd6c6a7a9eb50660f31444d9e4a3d0a586ad
kleshninpo <kleshnin.p.o@gmail.com>
2019-01-03 16:00:13 +0300

    Updates

---
+++ precache-manifest.a1971c706eb214324e54e4ad757b805b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0bfe7a68970dff092146",
+    "url": "/shoppage/static/js/runtime~main.0bfe7a68.js"
+  },
+  {
+    "revision": "b862f28532d1d526e823",
+    "url": "/shoppage/static/js/main.b862f285.chunk.js"
+  },
+  {
+    "revision": "118502f52e7f69e8dd21",
+    "url": "/shoppage/static/js/1.118502f5.chunk.js"
+  },
+  {
+    "revision": "b862f28532d1d526e823",
+    "url": "/shoppage/static/css/main.acc6f976.chunk.css"
+  },
+  {
+    "revision": "64b8a209f60a7f5ef6481c99c72c2a30",
+    "url": "/shoppage/index.html"
+  }
+];
